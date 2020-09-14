7f43bbf5a16c5d7d177d87281a237f09783f320b
shichemt <shichemt@live.com>
2020-09-14 02:35:43 -0500

    Bug fixes and new stuff

--- RSSTrigger.php
+++ RSSTrigger.php
@@ -2,48 +2,57 @@
 
 class RSSTrigger {
 
+    private $CACHE_DIR = __DIR__ . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR;
 
     private $filePrefix = "";
     private $feedUrl = "";
-    private const CACHE_DIR = __DIR__ . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR;
+    
 
 
     public function __construct($url, $filePrefix) {
         $this->filePrefix = $filePrefix;
         $this->feedUrl = $url;
 
-        // Create cache directory
-        if (!mkdir(CACHE_DIR, 0777, true)) {
-            throw("Error: Cannot create cache directory.");
+        // Create cache directory if it does not exist.
+        if (!file_exists($this->CACHE_DIR)) {
+            if (!mkdir($this->CACHE_DIR, 0777, true)) {
+                throw new Exception("Error: Cannot create cache directory.");
+            }
         }
     }
 
     private function findOldFile() {
         $fileArray = array();
-        foreach (glob(CACHE_DIR . $filePrefix . "*") as $file) {
+        $files = glob($this->CACHE_DIR . $this->filePrefix . "*");
+
+        foreach ($files as $file) {
             $fileArray[$file] = filemtime($file);
         }
-        $fileArray = asort($fileArray);
-        return (isset($fileArray[0]) ? $fileArray[0] : false);
+        arsort($fileArray);
+
+        return (!empty($fileArray) ? array_key_first($fileArray) : false);
     }
 
 
     /**
      * Saves the new xml file to local drive, and return the value of the user-defined function.
      * @param string $xpath: Path of the xml need to be parsed. example /a/b/c
+     * @param function comparisonMethod: user-predefined-function that compares values to be extracted.
      * @param function func_name: user-predefined-function that needs to be executed on the new elements.
      * @return mixed: Return value of func_name
      */
-    public function fetchFeed($xpath, $func_name) {
+    public function fetchFeed($xpath, $comparisonMethod, $func_name) {
         $feed = file_get_contents($this->feedUrl);
         if($feed === FALSE) {
-            throw("Error: Cannot fetch the url");
+            throw new Exception("Error: Cannot fetch the url");
         } else {
-            $newElements = $this->getNewUniqueElements($feed, $xpath);
-
-            if (file_put_contents(CACHE_DIR . $filePrefix . date("YmdHis"), $feed) === FALSE) {
-                throw("Error: Cannot save file.");
+            $newElements = $this->getNewUniqueElements($feed, $xpath, $comparisonMethod);
+            $newfileName = $this->CACHE_DIR . $this->filePrefix . date("YmdHis");
+            if (file_put_contents($newfileName, $feed) === FALSE) {
+                throw new Exception("Error: Cannot save file.");
             } 
+
+            chmod($newfileName, 0777);
 
             return $func_name($newElements);
         }
@@ -55,24 +64,25 @@ class RSSTrigger {
      * @param string $xpath: Path of the xml need to be parsed. example /a/b/c
      * @return array of strings: New unique elements.
      */
-    private function getNewUniqueElements($newFeed, $xpath) {
+    private function getNewUniqueElements($newFeed, $xpath, $func_name) {
 
         $newFeedXml = simplexml_load_string($newFeed, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $newFeedXml->registerXPathNamespace('x', 'http://www.w3.org/2005/Atom');
         $newFeedItems = (array) $newFeedXml->xpath($xpath);
 
         $oldFeed = $this->findOldFile();
+
         if (!!$oldFeed) {
             $oldFeedXml = simplexml_load_file($oldFeed, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $oldFeedXml->registerXPathNamespace('x', 'http://www.w3.org/2005/Atom');
             $oldFeedItems = (array) $oldFeedXml->xpath($xpath);
+
             $newfinalArray = array();
-            foreach ($newFeedItems as $newItem) {
-                if (!in_array($newItem, $oldFeedItems)) {
-                    array_push($newfinalArray, $newItem);
-                }
-            }
+            $newfinalArray = $func_name($newFeedItems, $oldFeedItems);
+
 
             // delete old feed once done fetching new unique elements.
-            unlink(CACHE_DIR . $oldFeed);
+            unlink($oldFeed);
 
             return $newfinalArray;
         }
